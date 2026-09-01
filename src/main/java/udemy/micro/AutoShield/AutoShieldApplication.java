d443adfae14361b3841d5f70bb46c349fc36dd55
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-09-01 20:03:20 -0300

    Inicial commit

---
+++ src/main/java/udemy/micro/AutoShield/AutoShieldApplication.java
@@ -0,0 +1,13 @@
+package udemy.micro.AutoShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/micro/AutoShield/AutoShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.micro.AutoShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
